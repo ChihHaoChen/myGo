81195e6ffe15677f4f6067c2d679276f83034e8d
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2020-07-14 00:50:10 +0800

    Update with another method.

--- answer.js
+++ answer.js
@@ -1,3 +1,6 @@
+/*
+ *  The 1st method
+ */
 
 function valueExtract(input) {
 	var arrTemp = [];
@@ -30,7 +33,7 @@ function createObject(base, arr) {
 }
 
 
-const reverse = (input) => {
+const reverse1 = (input) => {
     var output = {};
 
 	createObject(output, valueExtract(input));
@@ -38,4 +41,41 @@ const reverse = (input) => {
     return output;
 };
 
-module.exports = { reverse };
+/*
+ *  The 2nd method
+ */
+
+function reverse2Core(input, output) {
+    for(const [key, value] of Object.entries(input)) {
+		if (typeof(value) === 'object' ) {
+            if (Object.keys(output).length === 0) {
+                output = {key};
+            }
+            else if (Object.keys(output)[0] === "key") {
+                output[key] = output["key"];
+                delete output["key"];
+            }
+            else {
+                output = {[key]: output};
+            }
+
+            return reverse2Core(value, output);
+		}
+		else if (value !== undefined && value != null) {
+			output = {[key]:output};
+			output = {[value]:output};
+
+            return output;
+		}
+
+	}
+
+}
+
+
+const reverse2 = (input) => {
+    var output = {};
+    return reverse2Core(input, output);
+}
+
+module.exports = { reverse1, reverse2 };
--- answer.test.js
+++ answer.test.js
@@ -21,14 +21,27 @@ let outputValue = {
   }
 };
 
-const { reverse } = require('./answer');
+const { reverse1, reverse2 } = require('./answer');
 
+console.log("test another method => ", JSON.stringify(reverse2(inputValue)));
 
-test('Reversing inputValue should give outputValue', () => {
-  expect(reverse(inputValue)).toStrictEqual(outputValue);
+test('Reversing inputValue should give outputValue with the 1st method', () => {
+  expect(reverse1(inputValue)).toStrictEqual(outputValue);
 });
 
 
-test('Reversing inputValue twice should give InputValue', () => {
-  expect(reverse(reverse(inputValue))).toStrictEqual(inputValue);
+test('Reversing inputValue twice should give InputValue the 1st method', () => {
+  expect(reverse1(reverse1(inputValue))).toStrictEqual(inputValue);
 });
+
+
+test('Reversing inputValue should give outputValue with the 2nd method', () => {
+  expect(reverse2(inputValue)).toStrictEqual(outputValue);
+});
+
+
+test('Reversing inputValue twice should give InputValue the 2nd method', () => {
+  expect(reverse2(reverse2(inputValue))).toStrictEqual(inputValue);
+});
+
+
